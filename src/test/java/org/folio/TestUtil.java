3e4ec03e197396026fa03beb053a282cbf980820
Andrii_Novytskyi <Andrii_Novytskyi@epam.com>
2020-08-07 17:47:58 +0300

    MDEXP-255 Add function to default constructor

--- src/test/java/org/folio/TestUtil.java
+++ src/test/java/org/folio/TestUtil.java
@@ -1,16 +1,10 @@
 package org.folio;
 
-import io.vertx.core.json.JsonObject;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Objects;
 
 public final class TestUtil {
   public static String readFileContentFromResources(String path) {
