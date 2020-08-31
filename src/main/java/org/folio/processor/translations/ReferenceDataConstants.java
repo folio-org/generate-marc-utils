fb7c7df91f23fe68569bc26a15b26f9b7d69133c
Illia Borysenko <Illia_Borysenko@epam.com>
2020-08-31 15:23:05 +0300

    MDEXP-252 Applying mapping profile - location fields - holdings and item

--- src/main/java/org/folio/processor/translations/ReferenceDataConstants.java
+++ src/main/java/org/folio/processor/translations/ReferenceDataConstants.java
@@ -5,6 +5,9 @@ public class ReferenceDataConstants {
   public static final String IDENTIFIER_TYPES = "identifierTypes";
   public static final String CONTRIBUTOR_NAME_TYPES = "contributorNameTypes";
   public static final String LOCATIONS = "locations";
+  public static final String LIBRARIES = "loclibs";
+  public static final String CAMPUSES = "loccamps";
+  public static final String INSTITUTIONS = "locinsts";
   public static final String MATERIAL_TYPES = "materialTypes";
   public static final String MODE_OF_ISSUANCES = "issuanceModes";
   public static final String INSTANCE_TYPES = "instanceTypes";
