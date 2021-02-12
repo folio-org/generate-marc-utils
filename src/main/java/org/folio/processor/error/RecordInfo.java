b9b58f64f1a7531141e3919dbefb5789a26372c8
Andrii Novytskyi <60778653+andrii-novytskyi@users.noreply.github.com>
2021-02-12 16:20:43 +0200

    MDEXP-367 Add fields data to record info (#42)

--- src/main/java/org/folio/processor/error/RecordInfo.java
+++ src/main/java/org/folio/processor/error/RecordInfo.java
@@ -6,6 +6,8 @@ package org.folio.processor.error;
 public class RecordInfo {
   private String id;
   private RecordType type;
+  private String fieldName;
+  private String fieldValue;
 
   public RecordInfo(String id, RecordType type) {
     this.id = id;
@@ -19,4 +21,13 @@ public class RecordInfo {
   public RecordType getType() {
     return type;
   }
+
+  public void setFieldName(String fieldName) { this.fieldName = fieldName; }
+
+  public void setFieldValue(String fieldValue) { this.fieldValue = fieldValue; }
+
+  public String getFieldName() { return fieldName; }
+
+  public String getFieldValue() { return fieldValue; }
+
 }
